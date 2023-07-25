f725cafef38bdf1cc63e99a78f5b162df51986ff
Fernando Rodrigo Vila <fernandorodrigovila@gmail.com>
2023-07-25 17:44:37 -0300

    Persistance is now working.

--- src/main/java/com/tenpo/challenge/model/PercentageEntity.java
+++ src/main/java/com/tenpo/challenge/model/PercentageEntity.java
@@ -14,6 +14,7 @@ import java.time.Instant;
 @Table(name = "percentage_entity")
 public class PercentageEntity {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private Double percentage;
     private Instant timestamp;
--- src/main/java/com/tenpo/challenge/service/ExternalPercentageService.java
+++ src/main/java/com/tenpo/challenge/service/ExternalPercentageService.java
@@ -1,6 +1,7 @@
 package com.tenpo.challenge.service;
 
 import com.tenpo.challenge.model.PercentageEntity;
+import com.tenpo.challenge.repository.PercentageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.stereotype.Service;
@@ -15,12 +16,13 @@ public class ExternalPercentageService {
 
     private final WebClient webClient;
     private final ReactiveRedisTemplate<String, Double> redisTemplate;
-    Perc
+    private final PercentageRepository percentageRepository;
 
     @Autowired
-    public ExternalPercentageService(WebClient.Builder webClientBuilder, ReactiveRedisTemplate<String, Double> redisTemplate) {
+    public ExternalPercentageService(WebClient.Builder webClientBuilder, ReactiveRedisTemplate<String, Double> redisTemplate, PercentageRepository percentageRepository) {
         this.webClient = webClientBuilder.baseUrl("http://localhost:1080").build();
         this.redisTemplate = redisTemplate;
+        this.percentageRepository=percentageRepository;
     }
 
     public Mono<Double> getPercentage() {
@@ -30,7 +32,7 @@ public class ExternalPercentageService {
                         .bodyToMono(Double.class)
                         .retry(3)
                         .map(result -> new PercentageEntity(result, Instant.now()))
-                        .flatMap(percentageEntity -> )
-                        .flatMap(result -> redisTemplate.opsForValue().set("percentage", result, Duration.ofMinutes(30)).thenReturn(result)));
+                        .map(percentageEntity -> percentageRepository.save(percentageEntity) )
+                        .flatMap(percentageEntity -> redisTemplate.opsForValue().set("percentage", percentageEntity.getPercentage(), Duration.ofMinutes(30)).thenReturn(percentageEntity.getPercentage())));
     }
 }

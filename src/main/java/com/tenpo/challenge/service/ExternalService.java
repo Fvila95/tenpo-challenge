48d998498d3059fc4de7f9e5b110b333680c76fb
Fernando Rodrigo Vila <fernandorodrigovila@gmail.com>
2023-07-25 15:55:33 -0300

    Redis is implemented for caching the external service response.

--- src/main/java/com/tenpo/challenge/service/ExternalService.java
+++ src/main/java/com/tenpo/challenge/service/ExternalService.java
@@ -1,25 +1,31 @@
 package com.tenpo.challenge.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
 import java.util.Random;
 
 @Service
 public class ExternalService {
 
     private final WebClient webClient;
+    private final ReactiveRedisTemplate<String, Double> redisTemplate;
 
     @Autowired
-    public ExternalService(WebClient.Builder webClientBuilder) {
+    public ExternalService(WebClient.Builder webClientBuilder,ReactiveRedisTemplate<String, Double> redisTemplate) {
         this.webClient = webClientBuilder.baseUrl("http://localhost:1080").build();
+        this.redisTemplate = redisTemplate;
     }
 
     public Mono<Double> getPercentage() {
-        return webClient.get().uri("/percentage")
-                .retrieve()
-                .bodyToMono(Double.class);
+        return redisTemplate.opsForValue().get("percentage")
+                .switchIfEmpty(webClient.get().uri("/percentage")
+                        .retrieve()
+                        .bodyToMono(Double.class)
+                        .flatMap(result -> redisTemplate.opsForValue().set("percentage", result, Duration.ofMinutes(30)).thenReturn(result)));
     }
 }
